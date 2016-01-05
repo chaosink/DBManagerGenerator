3e768c4d363ea729c74d81b58e24f6f4cbf6a38e
chaosink <475936052@qq.com>
2016-01-05 13:57:02 +0800

    ..

--- ZJUCourse.java
+++ ZJUCourse.java
@@ -6,7 +6,7 @@ class SelectResult {
 	ArrayList<ArrayList<String>> record;
 
 	SelectResult(String attribute) {
-		this.attribute = attribute.split(" ");
+		this.attribute = attribute.replaceAll(",", " ").split(" +");
 		this.record = new ArrayList<ArrayList<String>>();
 	}
 }
@@ -46,11 +46,10 @@ public class ZJUCourse {
 				query += " where " + where;
 			ResultSet rs = statement.executeQuery(query);
 			while(rs.next()) {
-				ArrayList<String> row = new ArrayList<String>();
-				row.add(rs.getInt("course_id") + "");
-				row.add(rs.getInt("teacher_id") + "");
-				row.add(rs.getInt("classroom_id") + "");
-				sr.record.add(row);
+				ArrayList<String> record = new ArrayList<String>();
+				for(int i = 1; i <= sr.attribute.length; i++)
+					record.add(rs.getObject(i).toString());
+				sr.record.add(record);
 			}
 			return sr;
 		} catch(Exception e) {
@@ -59,7 +58,35 @@ public class ZJUCourse {
 		}
 	}
 
+	public void printSeperator(int[] width) {
+		for(int i = 0; i < width.length; i++) {
+			System.out.println("+");
+			for(int j = 0; j < width[i]; j++)
+				System.out.print("-");
+		}
+		System.out.println("+");
+	}
+
+	public void printRecord(ArrayList<String> record, int[] width) {
+		for(int i = 0; i < width.length; i++) {
+			System.out.println("|");
+			for(int j = 0; j < width[i]; j++) {
+				String format = "%" + width[i] + "s";
+				System.out.printf(format, record.get(i));
+			}
+		}
+		System.out.println("+");
+	}
+
 	public void display(SelectResult sr) {
+		int[] width = new int[sr.attribute.length];
+		for(int i = 0; i < sr.attribute.length; i++)
+			width[i] = sr.attribute[i].length();
+		for(int i = 0; i < sr.attribute.length; i++)
+			for(int j = 0; j < sr.record.size(); j++)
+				if(sr.record.get(j).get(i).length() > width[i])
+					width[i] = sr.record.get(j).get(i).length();
+
 		for(int i = 0; i < sr.attribute.length; i++)
 			System.out.print(sr.attribute[i] + " ");
 		System.out.println();
@@ -75,15 +102,6 @@ public class ZJUCourse {
 	public void insert(String record) {
 		try {
 			statement.executeUpdate("insert into " + table + " values (" + record + ")");
-//			statementInsert.setInt(1, record.course_id);
-//			statementInsert.setString(2, record.name);
-//			statementInsert.setInt(3, record.credit);
-//			statementInsert.setInt(4, record.teacher_id);
-//			statementInsert.setInt(5, record.classroom_id);
-//			statementInsert.setString(6, record.time);
-//			statementInsert.setString(7, record.semester);
-//			statementInsert.setString(8, record.introduction);
-//			statementInsert.executeUpdate();
 		} catch(Exception e) {
 			System.out.println(e);
 		}
@@ -91,16 +109,18 @@ public class ZJUCourse {
 
 	public void delete(String where) {
 		try {
-			statement.executeUpdate("delete from " + table + " where " + where);
+			String query = "delete from " + table;
+			if(!where.isEmpty())
+				query += " where " + where;
+			statement.executeUpdate(query);
 		} catch(Exception e) {
 			System.out.println(e);
 		}
 	}
 
 	public void update(String set, String where) {
-		String query = "update " + table + " set " + set + " where " + where;
 		try {
-			statement.executeUpdate(query);
+			statement.executeUpdate("update " + table + " set " + set + " where " + where);
 		} catch(Exception e) {
 			System.out.println(e);
 		}
--- ZJUCourseTest.java
+++ ZJUCourseTest.java
@@ -9,14 +9,10 @@ public class ZJUCourseTest {
 		zjuc.insert("2, '2', 2, 2, 2, '2', '2', '2'");
 		zjuc.insert("3, '3', 3, 3, 3, '3', '3', '3'");
 
-		ArrayList<String> attribute = new ArrayList<String>();
-		attribute.add("course_id");
-		attribute.add("teacher_id");
-		attribute.add("classroom_id");
-		SelectResult sr = zjuc.select(attribute, "course_id = 1 and teacher_id = 1 and classroom_id = 1");
+		SelectResult sr = zjuc.select("course_id, teacher_id, classroom_id", "course_id = 1 and teacher_id = 1 and classroom_id = 1");
 		zjuc.display(sr);
 		zjuc.update("course_id = 11, teacher_id = 11, classroom_id = 11", "course_id = 1");
-		sr = zjuc.select(attribute, "course_id = 11");
+		sr = zjuc.select("course_id, teacher_id, classroom_id", "course_id = 11");
 		zjuc.display(sr);
 
 		zjuc.delete("course_id = 11");
